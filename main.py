d65b3a8e58893bfbb18eebeeae2f9ee50debdde5
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-25 20:44:27 +0200

    project initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+
+with open('home.html','r') as html_file:
+    content = html_file.read()
+
+    soup = BeautifulSoup(content,'lxml')
+    tags = soup.find_all('h1')
+    print(tags)
