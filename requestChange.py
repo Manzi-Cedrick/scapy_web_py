701883301d42e90cbe022e2244bc622a988d0e8a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-26 00:41:31 +0200

    Request the jobs according to the given array length.

---
+++ requestChange.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
+                         '=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobsList = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+for jobs in jobsList:
+    published_date = jobs.find('span', class_='sim_posted').span.text
+    print(f"The published date: {published_date} :")
+    companyName = jobs.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+    skills = jobs.find('span', class_='srp-skills').text.replace(" ", "")
+    print(f"company name:{companyName}. Required skills: {skills}")
+    print(jobs)
