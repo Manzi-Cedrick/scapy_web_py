4d634a54be79080ccea3c54e48bc83e89e073029
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-26 00:41:03 +0200

    beatiful soup usage to prettify the data.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 
-with open('home.html','r') as html_file:
+with open('home.html', 'r') as html_file:
     content = html_file.read()
 
-    soup = BeautifulSoup(content,'lxml')
-    tags = soup.find_all('h1')
-    print(tags)
+    soup = BeautifulSoup(content, 'lxml')
+    course_cards = soup.find_all('div', class_="accordion")
+    for course_one in course_cards :
+        accordion_name = course_one.h2
+        print(accordion_name);
+    # print(course_cards);
